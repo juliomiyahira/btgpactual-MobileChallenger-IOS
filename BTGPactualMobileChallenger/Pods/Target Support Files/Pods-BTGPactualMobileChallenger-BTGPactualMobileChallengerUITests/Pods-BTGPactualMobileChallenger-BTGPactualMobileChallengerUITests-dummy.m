d81b2693014bb7a7a078b46166a354b7481ce0d8
julio cezar kenji miyahira <juliojapa@gmail.com>
2021-08-20 08:58:06 -0400

    [add] start coding

---
+++ BTGPactualMobileChallenger/Pods/Target Support Files/Pods-BTGPactualMobileChallenger-BTGPactualMobileChallengerUITests/Pods-BTGPactualMobileChallenger-BTGPactualMobileChallengerUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_BTGPactualMobileChallenger_BTGPactualMobileChallengerUITests : NSObject
+@end
+@implementation PodsDummy_Pods_BTGPactualMobileChallenger_BTGPactualMobileChallengerUITests
+@end
